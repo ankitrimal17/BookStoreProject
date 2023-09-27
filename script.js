7b6d0af9a19af8db2ad56c897ded93b042dfe4c3
ankitrimal17 <ankitrimal369@gmail.com>
2023-09-27 23:26:22 +0545

    Added to top button

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 // JavaScript to dynamically calculate animation duration based on text length
-const marqueeTextElements = document.querySelectorAll('.marquee-text');
+const marqueeTextElements = document.querySelectorAll(".marquee-text");
 marqueeTextElements.forEach((element) => {
-    const text = element.textContent.trim();
-    const textLength = text.length;
-    const animationDuration = textLength * 0.5 + 's'; // Adjust the factor as needed
-    element.style.setProperty('--animation-duration', animationDuration);
+  const text = element.textContent.trim();
+  const textLength = text.length;
+  const animationDuration = textLength * 0.5 + "s"; // Adjust the factor as needed
+  element.style.setProperty("--animation-duration", animationDuration);
 });
+
+let toTop = document.getElementById("goToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 250 ||
+    document.documentElement.scrollTop > 250
+  ) {
+    toTop.style.display = "grid";
+  } else {
+    toTop.style.display = "none";
+  }
+}
+//Goes to top of the page when button is clicked
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
